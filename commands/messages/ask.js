1e0c9203242c81142d3034b019b4282a5068e384
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-11 22:15:47 +0330

    Create ask.js

---
+++ commands/messages/ask.js
@@ -0,0 +1,101 @@
+const Discord = require('discord.js');
+const openAI = require('openai');
+const config = require('../../config.json');
+
+module.exports = {
+    name: "gpt",
+    aliases: ['ask', 'chat'],
+
+    async execute(client, message, args, cmd) {
+
+        await message.channel.sendTyping();
+
+        if (!args[0]) {
+
+            const embed = new Discord.EmbedBuilder()
+                .setColor(config.ErrorColor)
+                .setTitle('Error')
+                .setDescription(`You can't use the \`${cmd}\` command like this you have to provide something like the example\n\`\`\`\n${config.Prefix}${cmd} Explain loops in JavaScript.\n\`\`\``);
+
+            return await message.reply({ embeds: [embed] });
+
+        };
+
+        try {
+
+            const configuration = new openAI.Configuration({ apiKey: config.OpenAIapiKey });
+            const openai = new openAI.OpenAIApi(configuration);
+
+            const question = args.join(" ");
+            const prompt = `||>System: Instructions for ${client.user.username}: Please respond in a conversational and natural manner, as if you were having a conversation with a person. You are an advanced Discord Bot called ${client.user.username} developed by iTz Arshia in Javascript with Discord.js. Provide different stuff to assist in answering the task or question. Use appropriate discord markdown formatting to clearly distinguish syntax in your response.\n||>Messages:\n||>${message.author.username}: ${question}\n||>${client.user.username}:`;
+
+            openai.createCompletion({
+
+                model: 'text-davinci-003',
+                prompt: prompt,
+                max_tokens: 2000,
+                temperature: 1,
+                top_p: 0.9
+
+            }).then(async (response) => {
+
+                const answer = response.data.choices[0].text;
+                const usage = response.data.usage;
+
+                if (answer.length < 4096) {
+
+                    const embed = new Discord.EmbedBuilder()
+                        .setColor(config.MainColor)
+                        .setAuthor({
+                            name: question.length > 256 ? question.substring(0, 253) + "..." : question,
+                            iconURL: message.author.displayAvatarURL()
+                        })
+                        .setDescription(answer)
+                        .setFooter({
+                            text: `Consumed ${usage.total_tokens} (Q: ${usage.prompt_tokens} | A: ${usage.completion_tokens}) Tokens`,
+                            iconURL: client.user.displayAvatarURL()
+                        });
+
+                    await message.reply({ embeds: [embed] });
+
+                } else {
+
+                    const attachment = new Discord.AttachmentBuilder(
+                        Buffer.from(`${question}\n\n${answer}`, 'utf-8'),
+                        { name: 'response.txt' }
+                    );
+                    await message.reply({ files: [attachment] });
+
+                };
+
+            }).catch(async (error) => {
+
+                const embed = new Discord.EmbedBuilder()
+                    .setColor(config.ErrorColor)
+                    .setAuthor({
+                        name: question.length > 256 ? question.substring(0, 253) + "..." : question,
+                        iconURL: message.author.displayAvatarURL()
+                    })
+                    .setDescription(error.message);
+
+                await message.reply({ embeds: [embed] }).catch(() => null);
+
+            });
+
+        } catch (error) {
+
+            const embed = new Discord.EmbedBuilder()
+                .setColor(config.ErrorColor)
+                .setAuthor({
+                    name: question.length > 256 ? question.substring(0, 253) + "..." : question,
+                    iconURL: message.author.displayAvatarURL()
+                })
+                .setDescription(error.message);
+
+            await message.reply({ embeds: [embed] }).catch(() => null);
+
+        };
+
+    },
+
+};
