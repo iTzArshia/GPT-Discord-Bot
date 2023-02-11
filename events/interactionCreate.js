f53cbb3114d34006f8bb9a8472d0c2822ebb2305
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-11 22:30:42 +0330

    Create interactionCreate.js

---
+++ events/interactionCreate.js
@@ -0,0 +1,17 @@
+module.exports = async (client, interaction) => {
+
+    if (!interaction.isChatInputCommand() || !interaction.channel.permissionsFor(interaction.guild.members.me).has(["ViewChannel", "SendMessages", "EmbedLinks", "ReadMessageHistory"])) return;
+
+    const command = client.SlashCommands.get(interaction.commandName);
+    if (command) {
+
+        try {
+            command.execute(client, interaction);
+        } catch (error) {
+            console.error(error);
+            return await interaction.reply({ content: error.message.length > 4096 ? error.message.slice(0, 4093) + "..." : error.message, ephemeral: true }).catch(() => null);
+        };
+
+    };
+
+};
