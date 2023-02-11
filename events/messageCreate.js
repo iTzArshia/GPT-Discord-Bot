059a15a53fd80e16c63fba167152a5e1afff4df4
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-11 21:10:00 +0330

    Create messageCreate.js

---
+++ events/messageCreate.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+
+module.exports = async (client, message) => {
+
+    if (message.channel.type === Discord.ChannelType.DM || message.author.bot || message.system || !message.content.toLowerCase().startsWith(config.Prefix)) return;
+
+    const args = message.content.slice(config.Prefix.length).split(/ +/);
+    const cmd = args.shift().toLowerCase();
+    const command = client.MessageCommands.get(cmd) || client.MessageCommands.find(c => c.aliases && c.aliases.includes(cmd));
+    if (command) {
+
+        try {
+            command.execute(client, message, args, cmd);
+        } catch (error) {
+            console.error(error)
+        }
+
+    };
+
+};
