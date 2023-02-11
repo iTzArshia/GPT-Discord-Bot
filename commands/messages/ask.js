9660750a1c1c7f0b2523ced50bfd7785f523c901
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-11 23:46:17 +0330

    Update ask.js

--- commands/messages/ask.js
+++ commands/messages/ask.js
@@ -22,54 +22,56 @@ module.exports = {
 
         };
 
-        try {
+        const configuration = new openAI.Configuration({ apiKey: config.OpenAIapiKey });
+        const openai = new openAI.OpenAIApi(configuration);
 
-            const configuration = new openAI.Configuration({ apiKey: config.OpenAIapiKey });
-            const openai = new openAI.OpenAIApi(configuration);
+        const question = args.join(" ");
+        const prompt = `||>System: Instructions for ${client.user.username}: Please respond in a conversational and natural manner, as if you were having a conversation with a person. You are an advanced Discord Bot called ${client.user.username} developed by iTz Arshia in Javascript with Discord.js. Provide different stuff to assist in answering the task or question. Use appropriate discord markdown formatting to clearly distinguish syntax in your response.\n||>Messages:\n||>${message.author.username}: ${question}\n||>${client.user.username}:`;
 
-            const question = args.join(" ");
-            const prompt = `||>System: Instructions for ${client.user.username}: Please respond in a conversational and natural manner, as if you were having a conversation with a person. You are an advanced Discord Bot called ${client.user.username} developed by iTz Arshia in Javascript with Discord.js. Provide different stuff to assist in answering the task or question. Use appropriate discord markdown formatting to clearly distinguish syntax in your response.\n||>Messages:\n||>${message.author.username}: ${question}\n||>${client.user.username}:`;
+        openai.createCompletion({
 
-            openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: prompt,
+            max_tokens: 2000,
+            temperature: 1,
+            top_p: 0.9
 
-                model: 'text-davinci-003',
-                prompt: prompt,
-                max_tokens: 2000,
-                temperature: 1,
-                top_p: 0.9
+        }).then(async (response) => {
 
-            }).then(async (response) => {
+            const answer = response.data.choices[0].text;
+            const usage = response.data.usage;
 
-                const answer = response.data.choices[0].text;
-                const usage = response.data.usage;
+            if (answer.length < 4096) {
 
-                if (answer.length < 4096) {
+                const embed = new Discord.EmbedBuilder()
+                    .setColor(config.MainColor)
+                    .setAuthor({
+                        name: question.length > 256 ? question.substring(0, 253) + "..." : question,
+                        iconURL: message.author.displayAvatarURL()
+                    })
+                    .setDescription(answer)
+                    .setFooter({
+                        text: `Consumed ${usage.total_tokens} (Q: ${usage.prompt_tokens} | A: ${usage.completion_tokens}) Tokens`,
+                        iconURL: client.user.displayAvatarURL()
+                    });
 
-                    const embed = new Discord.EmbedBuilder()
-                        .setColor(config.MainColor)
-                        .setAuthor({
-                            name: question.length > 256 ? question.substring(0, 253) + "..." : question,
-                            iconURL: message.author.displayAvatarURL()
-                        })
-                        .setDescription(answer)
-                        .setFooter({
-                            text: `Consumed ${usage.total_tokens} (Q: ${usage.prompt_tokens} | A: ${usage.completion_tokens}) Tokens`,
-                            iconURL: client.user.displayAvatarURL()
-                        });
+                await message.reply({ embeds: [embed] });
 
-                    await message.reply({ embeds: [embed] });
+            } else {
 
-                } else {
+                const attachment = new Discord.AttachmentBuilder(
+                    Buffer.from(`${question}\n\n${answer}`, 'utf-8'),
+                    { name: 'response.txt' }
+                );
+                await message.reply({ files: [attachment] });
 
-                    const attachment = new Discord.AttachmentBuilder(
-                        Buffer.from(`${question}\n\n${answer}`, 'utf-8'),
-                        { name: 'response.txt' }
-                    );
-                    await message.reply({ files: [attachment] });
+            };
 
-                };
+        }).catch(async (error) => {
 
-            }).catch(async (error) => {
+            console.error(error);
+
+            if (error.message) {
 
                 const embed = new Discord.EmbedBuilder()
                     .setColor(config.ErrorColor)
@@ -81,21 +83,9 @@ module.exports = {
 
                 await message.reply({ embeds: [embed] }).catch(() => null);
 
-            });
+            };
 
-        } catch (error) {
-
-            const embed = new Discord.EmbedBuilder()
-                .setColor(config.ErrorColor)
-                .setAuthor({
-                    name: question.length > 256 ? question.substring(0, 253) + "..." : question,
-                    iconURL: message.author.displayAvatarURL()
-                })
-                .setDescription(error.message);
-
-            await message.reply({ embeds: [embed] }).catch(() => null);
-
-        };
+        });
 
     },
 
