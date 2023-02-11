afbefe5cf01e18d232b98b029136407b0b550c9d
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-11 22:28:59 +0330

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,7 +3,7 @@ const config = require('../config.json');
 
 module.exports = async (client, message) => {
 
-    if (message.channel.type === Discord.ChannelType.DM || message.author.bot || message.system || !message.content.toLowerCase().startsWith(config.Prefix)) return;
+    if (message.channel.type === Discord.ChannelType.DM || message.author.bot || message.system || !message.content.toLowerCase().startsWith(config.Prefix) || !message.channel.permissionsFor(message.guild.members.me).has(["ViewChannel", "SendMessages", "EmbedLinks", "ReadMessageHistory"])) return;
 
     const args = message.content.slice(config.Prefix.length).split(/ +/);
     const cmd = args.shift().toLowerCase();
