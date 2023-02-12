95a7becef0d17c7f6d37890da1834ad09cb7897b
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-12 11:41:33 +0330

    Update interactionCreate.js
    Add Chalk

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,3 +1,5 @@
+const chalk = require('chalk');
+
 module.exports = async (client, interaction) => {
 
     if (!interaction.isChatInputCommand() || !interaction.channel.permissionsFor(interaction.guild.members.me).has(["ViewChannel", "SendMessages", "EmbedLinks", "ReadMessageHistory"])) return;
@@ -8,7 +10,7 @@ module.exports = async (client, interaction) => {
         try {
             command.execute(client, interaction);
         } catch (error) {
-            console.error(error);
+            console.error(chalk.bold.redBright(error));
             return await interaction.reply({ content: error.message.length > 4096 ? error.message.slice(0, 4093) + "..." : error.message, ephemeral: true }).catch(() => null);
         };
 
