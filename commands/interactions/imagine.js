4b94bdaf80b5a124be3ebe1f0b49729cf9d5c93e
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-12 14:50:07 +0330

    Update imagine.js
    Update description and data

--- commands/interactions/imagine.js
+++ commands/interactions/imagine.js
@@ -30,7 +30,7 @@ module.exports = {
 
         }).then(async (response) => {
 
-            const data = response.data;
+            const data = response.data.data;
 
             const embeds = [
 
@@ -41,7 +41,8 @@ module.exports = {
                         name: question.length > 256 ? question.substring(0, 253) + "..." : question,
                         iconURL: interaction.user.displayAvatarURL()
                     })
-                    .setImage(data.data[0].url)
+                    .setDescription(data.map((d, i) => `[\`image ${i + 1}\`](${d.url})`).join(' | '))
+                    .setImage(data[0].url)
 
             ];
 
@@ -49,7 +50,7 @@ module.exports = {
 
                 const embed = new Discord.EmbedBuilder()
                     .setURL('https://github.com/iTzArshia/GPT-Discord-Bot')
-                    .setImage(data.data[i + 1].url);
+                    .setImage(data[i + 1].url);
 
                 embeds.push(embed);
 
