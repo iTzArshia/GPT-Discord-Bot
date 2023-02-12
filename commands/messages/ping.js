5f116e233e35ce4c6b64152bbf5fffd62983adf2
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-12 11:17:46 +0330

    Create ping.js

---
+++ commands/messages/ping.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+    name: "Ping",
+    aliases: ["latency"],
+    description: "Shows the bot\'s latency.",
+
+    async execute(client, message, args, cmd) {
+
+        const embed = new Discord.EmbedBuilder()
+            .setColor(config.MainColor)
+            .setAuthor({
+                name: `Pong!`,
+                iconURL: client.user.displayAvatarURL({ size: 1024 })
+            })
+            .addFields(
+                {
+                    name: `📡 Ping:`,
+                    value: `${client.ws.ping}ms`,
+                    inline: true
+                },
+                {
+                    name: `💾 Memory:`,
+                    value: `${Math.round((process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}MB`,
+                    inline: true
+                },
+                {
+                    name: `⏳ Uptime:`,
+                    value: `<t:${Math.trunc(client.readyTimestamp / 1000)}:D> | <t:${Math.trunc(client.readyTimestamp / 1000)}:R>`,
+                    inline: false
+                },
+            )
+            .setFooter({
+                text: `Commanded by ${message.author.tag}`,
+                iconURL: message.author.displayAvatarURL({ size: 1024 })
+            });
+
+        return await message.reply({ embeds: [embed] });
+
+    },
+
+};
