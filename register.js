b70fa805a566b0deddd3c46aa554a7f4ba996b2a
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-12 11:29:39 +0330

    Update register.js
    Add chalk and invite url with "applications.commands" scope

--- register.js
+++ register.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const chalk = require('chalk');
 const fs = require('node:fs');
 const config = require('./config.json');
 const commands = [];
@@ -16,14 +17,15 @@ const rest = new Discord.REST({ version: '10' }).setToken(config.Token);
 
     try {
 
-        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+        console.log(chalk.bold.yellowBright(`Started refreshing ${commands.length} application (/) commands.`));
 
         const data = await rest.put(
             Discord.Routes.applicationCommands(config.ClientID),
             { body: commands },
         );
 
-        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+        console.log(chalk.bold.greenBright(`Successfully reloaded ${data.length} application (/) commands.`));
+        console.log(chalk.bold.redBright(`Note: if you didn't see slash commands in your server maybe your bot don't have "applicatiton.commands" scope try to invite it using this link\nhttps://discord.com/api/oauth2/authorize?client_id=${config.ClientID}&permissions=0&scope=bot%20applications.commands`))
 
     } catch (error) {
         console.error(error);
