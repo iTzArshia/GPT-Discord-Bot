a4b448fc81c37a7ac7cc0604eef825285c5511a6
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-14 00:04:29 +0330

    Update translate.js
    add Moderation check for prompts with Moderation model

--- commands/interactions/translate.js
+++ commands/interactions/translate.js
@@ -42,48 +42,102 @@ module.exports = {
         const openai = new openAI.OpenAIApi(configuration);
 
         const question = interaction.options.getString("prompt");
-        const language = interaction.options.getString("language") || 'English';
 
-        const prompt = `I want you to act as an ${language} translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in ${language}. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level ${language} words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My first sentence is “${question}”`;
+        openai.createModeration({
 
-        openai.createCompletion({
-
-            model: 'text-davinci-003',
-            prompt: prompt,
-            max_tokens: 2048,
-            temperature: 0.77,
-            top_p: 0.9,
-            frequency_penalty: 0.95,
-            presence_penalty: 0.95
+            input: question
 
         }).then(async (response) => {
 
-            const answer = response.data.choices[0].text;
-            const usage = response.data.usage;
+            const data = response.data.results[0];
+            if (data.flagged) {
 
-            if (answer.length < 4096) {
+                function replaces(string) {
+                    return string
+                        .replace('sexual', 'Sexual')
+                        .replace('hate', 'Hate')
+                        .replace('violence', 'Violence')
+                        .replace('self-harm', 'Self-Harm')
+                        .replace('sexual/minors', 'Sexual/Minors')
+                        .replace('hate/threatening', 'Hate/Threatening')
+                        .replace('violence/graphic', 'Violence/Graphic')
+                };
 
-                const embed = new Discord.EmbedBuilder()
-                    .setColor(config.MainColor)
+                const logEmbed = new Discord.EmbedBuilder()
+                    .setColor(config.ErrorColor)
                     .setAuthor({
                         name: question.length > 256 ? question.substring(0, 253) + "..." : question,
                         iconURL: interaction.user.displayAvatarURL()
                     })
-                    .setDescription(answer)
-                    .setFooter({
-                        text: `Consumed ${usage.total_tokens} (Q: ${usage.prompt_tokens} | A: ${usage.completion_tokens}) Tokens`,
-                        iconURL: client.user.displayAvatarURL()
-                    });
+                    .setDescription(`Your request was rejected as a result of our safty system. Your prompt may contain text that is not allowd by our safty system\n\n**Flags:** ${Object.keys(data.categories).filter(key => data.categories[key]).map(flag => replaces(flag)).join(", ")}`);
 
-                await interaction.editReply({ embeds: [embed] });
+                return interaction.editReply({ embeds: [logEmbed] });
 
             } else {
 
-                const attachment = new Discord.AttachmentBuilder(
-                    Buffer.from(`${question}\n\n${answer}`, 'utf-8'),
-                    { name: 'response.txt' }
-                );
-                await interaction.editReply({ files: [attachment] });
+                const language = interaction.options.getString("language") || 'English';
+                const prompt = `I want you to act as an ${language} translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in ${language}. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level ${language} words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My first sentence is “${question}”`;
+
+                openai.createCompletion({
+
+                    model: 'text-davinci-003',
+                    prompt: prompt,
+                    max_tokens: 2048,
+                    temperature: 0.77,
+                    top_p: 0.9,
+                    frequency_penalty: 0.95,
+                    presence_penalty: 0.95
+
+                }).then(async (response) => {
+
+                    const answer = response.data.choices[0].text;
+                    const usage = response.data.usage;
+
+                    if (answer.length < 4096) {
+
+                        const embed = new Discord.EmbedBuilder()
+                            .setColor(config.MainColor)
+                            .setAuthor({
+                                name: question.length > 256 ? question.substring(0, 253) + "..." : question,
+                                iconURL: interaction.user.displayAvatarURL()
+                            })
+                            .setDescription(answer)
+                            .setFooter({
+                                text: `Consumed ${usage.total_tokens} (Q: ${usage.prompt_tokens} | A: ${usage.completion_tokens}) Tokens`,
+                                iconURL: client.user.displayAvatarURL()
+                            });
+
+                        await interaction.editReply({ embeds: [embed] });
+
+                    } else {
+
+                        const attachment = new Discord.AttachmentBuilder(
+                            Buffer.from(`${question}\n\n${answer}`, 'utf-8'),
+                            { name: 'response.txt' }
+                        );
+                        await interaction.editReply({ files: [attachment] });
+
+                    };
+
+                }).catch(async (error) => {
+
+                    console.error(chalk.bold.redBright(error));
+
+                    if (error.message) {
+
+                        const embed = new Discord.EmbedBuilder()
+                            .setColor(config.ErrorColor)
+                            .setAuthor({
+                                name: question.length > 256 ? question.substring(0, 253) + "..." : question,
+                                iconURL: interaction.user.displayAvatarURL()
+                            })
+                            .setDescription(error.message);
+
+                        await interaction.editReply({ embeds: [embed] }).catch(() => null);
+
+                    };
+
+                });
 
             };
 
@@ -91,22 +145,7 @@ module.exports = {
 
             console.error(chalk.bold.redBright(error));
 
-            if (error.message) {
-
-                const embed = new Discord.EmbedBuilder()
-                    .setColor(config.ErrorColor)
-                    .setAuthor({
-                        name: question.length > 256 ? question.substring(0, 253) + "..." : question,
-                        iconURL: interaction.user.displayAvatarURL()
-                    })
-                    .setDescription(error.message);
-
-                await interaction.editReply({ embeds: [embed] }).catch(() => null);
-
-            };
-
         });
-
 
     },
 
