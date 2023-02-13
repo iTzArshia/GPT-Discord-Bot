5091bf2de51a5501c48e5d24ff046eb7d8d29dcd
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-13 20:56:38 +0330

    Update translate.js
    Update prompt and add new languages to translate in

--- commands/interactions/translate.js
+++ commands/interactions/translate.js
@@ -6,10 +6,42 @@ const config = require('../../config.json');
 module.exports = {
     data: new Discord.SlashCommandBuilder()
         .setName("translate")
-        .setDescription("Translate your texts from any language to English!")
+        .setDescription("Translate your texts from any language to any language!")
         .addStringOption(option => option
             .setName("prompt")
             .setDescription("What is your text?")
+            .setRequired(true)
+        )
+        .addStringOption(option => option
+            .setName("language")
+            .setDescription("What language would you like me to translate your prompt into?")
+            .setChoices(
+                { name: 'English', value: 'English' },
+                { name: 'Spanish', value: 'Spanish' },
+                { name: 'French', value: 'French' },
+                { name: 'Chinese', value: 'Chinese' },
+                { name: 'Bulgarian', value: 'Bulgarian' },
+                { name: 'Czech', value: 'Czech' },
+                { name: 'Danish', value: 'Danish' },
+                { name: 'German', value: 'German' },
+                { name: 'Greek', value: 'Greek' },
+                { name: 'Finnish', value: 'Finnish' },
+                { name: 'Hungarian', value: 'Hungarian' },
+                { name: 'Indonesian', value: 'Indonesian' },
+                { name: 'Italian', value: 'Italian' },
+                { name: 'Japanese', value: 'Japanese' },
+                { name: 'Lithuanian', value: 'Lithuanian' },
+                { name: 'Latvian', value: 'Latvian' },
+                { name: 'Dutch', value: 'Dutch' },
+                { name: 'Polish', value: 'Polish' },
+                { name: 'Portuguese', value: 'Portuguese' },
+                { name: 'Romanian', value: 'Romanian' },
+                { name: 'Russian', value: 'Russian' },
+                { name: 'Slovak', value: 'Slovak' },
+                { name: 'Swedish', value: 'Swedish' },
+                { name: 'Turkish', value: 'Turkish' },
+                { name: 'Ukrainian', value: 'Ukrainian' }
+            )
             .setRequired(true)
         ),
 
@@ -21,7 +53,9 @@ module.exports = {
         const openai = new openAI.OpenAIApi(configuration);
 
         const question = interaction.options.getString("prompt");
-        const prompt = `||>System: Instructions for ${client.user.username}: Please act as an English translator. spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it to English and then answer in the corrected and improved version of my text in English. I want you to replace my simplified words and sentences with more beautiful and elegant, upper level English words and sentences. Retain the meaning, but elevate them into a higher literacy competency. I want you to only reply to the correction, the improvements and nothing else, do not write any additional explanations.\n||>Messages:\n||>${interaction.user.username}: ${question}\n||>${client.user.username}:`;
+        const language = interaction.options.getString("language") || 'English';
+
+        const prompt = `I want you to act as an ${language} translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in ${language}. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level ${language} words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My first sentence is “${question}”`;
 
         openai.createCompletion({
 
