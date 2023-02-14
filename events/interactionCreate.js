13d5a362004bf0217429b1569cdac9a7d324a5d3
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-15 00:59:08 +0330

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -661,13 +661,6 @@ module.exports = async (client, interaction) => {
 
             };
 
-            try {
-                command.execute(client, interaction);
-            } catch (error) {
-                console.error(chalk.bold.redBright(error));
-                return await interaction.reply({ content: error.message.length > 4096 ? error.message.slice(0, 4093) + "..." : error.message, ephemeral: true }).catch(() => null);
-            };
-
         };
 
     };
