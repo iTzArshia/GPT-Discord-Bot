56a835ff30517f6e831fd6608b0140f200cdb2e0
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-15 00:26:36 +0330

    Update translate.js
    Update prompt

--- commands/messages/translate.js
+++ commands/messages/translate.js
@@ -51,7 +51,7 @@ module.exports = {
 
             } else {
 
-                const prompt = `I want you to act as an English translator, spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations. My first sentence is “${question}”`;
+                const prompt = `System: Instructions for ${client.user.username}: Please act as an English translator. spelling corrector and improver. I will speak to you in any language and you will detect the language, translate it to English and then answer in the corrected and improved version of my text in English. I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences. Retain the meaning, but elevate them into a higher literacy competency. I want you to only reply to the correction, the improvements and nothing else, do not write any additional explanations.\nMessages:\n- ${message.author.username}: ${question}\n- ${client.user.username}:`
 
                 openai.createCompletion({
 
