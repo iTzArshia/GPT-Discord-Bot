c924c5fb1c8c04cf3a99506b3302b56a30407a20
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-15 12:46:19 +0330

    Update moderation.js

--- configs/moderation.js
+++ configs/moderation.js
@@ -2,50 +2,53 @@ module.exports.moderation = {
     // True: Enable the Moderation model
     // False: Disable the Moderation model
     State: false,
-    // a list of ignored channels IDs which bot ignore messages there
+    // A list of channel IDs that the bot ignores messages from.
     IgnoredChannels: ["0000000000000000000", "1111111111111111111", "2222222222222222222"],
-    // Log's Channel ID it could be Text/Announcement/Voice/Thread/Post Channel ID
+    // Log's channel ID. It could be a text, announcement, voice, thread or post channel ID. 
+
     LogChannel: "0000000000000000000",
-    // Log's Embed Color it could be a #HexCode or resolvable text like: "Red"
+    // Log's embed color. It could be a #HexCode or resolvable text like: "Red"
     LogColor: "Red",
-    // a list of roles which can use moderation buttons without having permissions
+    // A list of roles that can use moderation buttons without having permissions. 
     AdminRoles: ["0000000000000000000", "1111111111111111111", "2222222222222222222"],
-    // a list of users which can use moderation buttons without having permissions
+    // A list of users that can use moderation buttons without having permissions.
     AdminUsers: ["0000000000000000000", "1111111111111111111", "2222222222222222222"],
     // True: Deletes Flagged Messages automatically
-    // False: Don't Delete Flagged Messages automatically
+    // False: Doesn't delete flagged messages automatically.
     AutoDelete: {
         "Sexual": false,
         "Hate": false,
         "Violence": false,
         "Self-Harm": false
     },
-    // True: Punish Flagged Messages authors automatically
-    // False: Don't Punish Flagged Messages authors automatically
+    // True: Punishes flagged messages' authors automatically.
+    // False: Doesn't punish flagged messages' authors automatically.
     AutoPunish: {
         "Sexual": false,
         "Hate": false,
         "Violence": false,
         "Self-Harm": false
     },
-    // Valid Punish Types for Flagged Messages authors if "AutoPunish" enabled: "Timeout", "Kick", "Ban" 
-    // note: if more than 1 flag enabled together it will first check for Ban then check for Kick and then check for Timeout
+    // Valid punishment types for flagged messages' authors if "AutoPunish" is enabled: "Timeout", "Kick", "Ban"
+    // Note: If more than 1 flag is enabled, the priority will be as follows: 1. Ban 2. Kick. 3. Timeout 
     AutoPunishType: {
         "Sexual": "Timeout",
         "Hate": "Timeout",
         "Violence": "Timeout",
         "Self-Harm": "Timeout"
     },
-    // For Timeout it will be "Timeout" duration
-    // For Ban it will be Delete Messages in past days duration
-    // For Kick it don't do anything
-    // Valid Timeout Durations: 1 Minute - 28 Days
-    // Valid Ban Delete Messages Durations: 0 seconds - 7 days
-    // m / min / minute / minutes 
-    // h / hour / hours
-    // d / day / days
-    // examples: "30m" / "1h" / "1d"  and ...
-    // note: if more than 1 flag enabled together it will check for the longest duration
+    // For timeouts it will be the timeout's duration 
+    // For bans, it will be the number of days from which it will purge the message history.
+    // For kick it doesn't do anything.
+    // Valid timeout duration: 1 Minute - 28 Days
+    // Valid ban delete messages duration: 0 Seconds - 7 Days
+    // Note: If more than 1 flag is enabled, it'll check for the longest duration.
+    // Eexamples: 
+    // Second: (1s / 10sec / 30secs / 60second / 120seconds)
+    // Minute: (1m / 10min / 30mins / 60minute / 120minutes)
+    // Hour: (1h / 2hr / 3hrs / 4hour / 5hours)
+    // Day: (1d / 2day / 3days)
+    // Week: (1w / 2week / 3weeks)
     AutoPunishDuration: {
         "Sexual": "1d",
         "Hate": "1d",
