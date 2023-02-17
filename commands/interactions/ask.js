4a2c7f0b43d7af514dc783c978cbff47944a8db8
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-17 22:29:44 +0330

    Update ask.js
    Add pricing to footers instead of consumed tokens

--- commands/interactions/ask.js
+++ commands/interactions/ask.js
@@ -95,7 +95,11 @@ module.exports = {
                                     name: question.length > 256 ? question.substring(0, 253) + "..." : question,
                                     iconURL: message.author.displayAvatarURL()
                                 })
-                                .setDescription(`Your request was rejected as a result of our safety system. Your prompt may contain text that is not allowd by our safety system\n\n**Flags:** ${func.flagCheck(data.categories).trueFlags}`);
+                                .setDescription(`Your request was rejected as a result of our safety system. Your prompt may contain text that is not allowd by our safety system\n\n**Flags:** ${func.flagCheck(data.categories).trueFlags}`)
+                                .setFooter({
+                                    text: `Costs ${func.pricing('davinci', usage.total_tokens)}`,
+                                    iconURL: client.user.displayAvatarURL()
+                                });
 
                             return message.reply({ embeds: [embed] });
 
@@ -111,7 +115,7 @@ module.exports = {
                                     })
                                     .setDescription(answer)
                                     .setFooter({
-                                        text: `Consumed ${usage.total_tokens} (Q: ${usage.prompt_tokens} | A: ${usage.completion_tokens}) Tokens`,
+                                        text: `Costs ${func.pricing('davinci', usage.total_tokens)}`,
                                         iconURL: client.user.displayAvatarURL()
                                     });
 
@@ -141,7 +145,11 @@ module.exports = {
                                     name: question.length > 256 ? question.substring(0, 253) + "..." : question,
                                     iconURL: message.author.displayAvatarURL()
                                 })
-                                .setDescription(error.message);
+                                .setDescription(error.message)
+                                .setFooter({
+                                    text: `Costs ${func.pricing('davinci', usage.total_tokens)}`,
+                                    iconURL: client.user.displayAvatarURL()
+                                });
 
                             await message.reply({ embeds: [embed] }).catch(() => null);
 
