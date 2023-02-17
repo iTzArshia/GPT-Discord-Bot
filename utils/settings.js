0d6b74ff863c01d912c8eeb4f0d978a51f6272ec
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-18 00:02:53 +0330

    Create options.js

---
+++ utils/settings.js
@@ -0,0 +1,28 @@
+module.exports.chatGPT = {
+    model: "text-davinci-003",
+    temprature: 0.9,
+    top_p: 1,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.6
+};
+module.exports.chatGPTask = {
+    model: "text-davinci-003",
+    temprature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0
+};
+module.exports.translator = {
+    model: "text-davinci-003",
+    temprature: 0.77,
+    top_p: 0.9,
+    frequency_penalty: 0.95,
+    presence_penalty: 0.95
+};
+module.exports.optimzer = {
+    model: "text-davinci-003",
+    temprature: 1,
+    top_p: 1,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0
+};
