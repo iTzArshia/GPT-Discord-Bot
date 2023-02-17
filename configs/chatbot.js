9c30b609e65a26bcb67e6fa129e7359365e900b4
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-18 00:13:26 +0330

    Create chatbot.js

---
+++ configs/chatbot.js
@@ -0,0 +1,14 @@
+module.exports.chatbot = {
+    // true: Enable the chatbot model.
+    // false: Disable the chatbot model.
+    State: false,
+    // ChatBot's channel ID. It could be a text, announcement, voice, thread or post channel ID.
+    ChatBotChannel: "0000000000000000000",
+    // A list of user IDs that the bot ignores messages from them.
+    IgnoredUsers: [
+        "0000000000000000000",
+        "1111111111111111111",
+        "2222222222222222222"
+    ],
+
+};
