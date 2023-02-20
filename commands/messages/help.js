a7531a6d4acc91ed60aa612723ed659fafc8cfb6
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-20 22:18:29 +0330

    Update help.js

--- commands/messages/help.js
+++ commands/messages/help.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const config = require('../../configs/config.json');
+const { chatbot } = require('../../configs/chatbot');
 
 module.exports = {
   name: "Help",
@@ -7,7 +8,7 @@ module.exports = {
   description: "Shows This!",
 
   async execute(client, message, args, cmd) {
- 
+
     await message.channel.sendTyping();
 
     const helpEmbed = new Discord.EmbedBuilder()
@@ -18,6 +19,12 @@ module.exports = {
       })
       .setDescription(client.MessageCommands.map(c => `> \`${config.Prefix}${c.name}\` \`(${c.aliases?.map(a => `${config.Prefix}${a}`)?.join(' / ') || 'No Aliases'})\`\n> *${c.description}*`).join('\n\n'))
       .setFooter({ text: 'Developed by iTz Arshia#7650 https://github.com/iTzArshia/GPT-Discord-Bot' });
+
+    if (chatbot.State) helpEmbed.addFields({
+      name: 'ChatBot:',
+      value: `Channel: <#${chatbot.ChatBotChannel}>`,
+      inline: true
+    });
 
     return await message.reply({ embeds: [helpEmbed] });
 
