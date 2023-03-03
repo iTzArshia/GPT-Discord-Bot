87797dc38c14b9612c48262ad1f941bad286919b
iTzArshia <iTz.Arshia80@Gmail.com>
2023-03-04 01:37:46 +0330

    Update settings.js

--- utils/settings.js
+++ utils/settings.js
@@ -1,19 +1,16 @@
-module.exports.chatGPT = {
-    model: "text-davinci-003",
+module.exports.completion = {
     temprature: 0.7,
     top_p: 1,
     frequency_penalty: 0.0,
     presence_penalty: 0.0
 };
 module.exports.translator = {
-    model: "text-davinci-003",
     temprature: 0.77,
     top_p: 0.9,
     frequency_penalty: 0.95,
     presence_penalty: 0.95
 };
 module.exports.optimzer = {
-    model: "text-davinci-003",
     temprature: 0.9,
     top_p: 1,
     frequency_penalty: 0.0,
