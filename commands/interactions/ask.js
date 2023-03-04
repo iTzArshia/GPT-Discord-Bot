0dc93aba262f30c913f1ccc48da48485e24d3503
iTzArshia <iTz.Arshia80@Gmail.com>
2023-03-04 15:20:30 +0330

    Update ask.js

--- commands/interactions/ask.js
+++ commands/interactions/ask.js
@@ -103,7 +103,7 @@ module.exports = {
         const moderationData = moderation.data.results[0];
         if (moderationData.flagged) {
 
-            const logEmbed = new Discord.EmbedBuilder()
+            const embed = new Discord.EmbedBuilder()
                 .setColor(config.ErrorColor)
                 .setAuthor({
                     name: question.length > 256 ? question.substring(0, 253) + "..." : question,
@@ -111,7 +111,7 @@ module.exports = {
                 })
                 .setDescription(`Your request was rejected as a result of our safety system. Your prompt may contain text that is not allowd by our safety system\n\n**Flags:** ${func.flagCheck(moderationData.categories).trueFlags}`);
 
-            await interaction.editReply({ embeds: [logEmbed] });
+            await interaction.editReply({ embeds: [embed] });
 
         } else {
 
@@ -293,7 +293,7 @@ module.exports = {
                         iconURL: client.user.displayAvatarURL()
                     });
 
-                return interaction.editReply({ embeds: [embed] });
+                await interaction.editReply({ embeds: [embed] });
 
             } else {
 
